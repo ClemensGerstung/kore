12eaf84fb2a79e5212907064a17e76b56cb3c26b
Clemens <clemens8496@gmail.com>
2015-09-21 23:07:47 +0200

    login improved in service

--- app/src/main/java/com/typingsolutions/passwordmanager/receiver/LoginReceiver.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/receiver/LoginReceiver.java
@@ -5,6 +5,9 @@ import android.content.Context;
 import android.content.Intent;
 
 public class LoginReceiver extends BroadcastReceiver {
+    // TODO: update UI!!!
+    // TODO: notification
+
     @Override
     public void onReceive(Context context, Intent intent) {
 
