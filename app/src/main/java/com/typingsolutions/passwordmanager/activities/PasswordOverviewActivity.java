9134506cdf738076dfe6040b1331928a3c101620
Clemens <clemens8496@gmail.com>
2015-09-23 23:21:27 +0200

    passwordoverview improved

--- app/src/main/java/com/typingsolutions/passwordmanager/activities/PasswordOverviewActivity.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/activities/PasswordOverviewActivity.java
@@ -8,39 +8,106 @@ import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.internal.widget.ListViewCompat;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.TextView;
 import com.typingsolutions.passwordmanager.R;
 import com.typingsolutions.passwordmanager.callbacks.AddPasswordCallback;
+import core.AsyncPasswordLoader;
+import core.DatabaseProvider;
+import core.Password;
+import core.UserProvider;
+import core.adapter.PasswordOverviewAdapter;
 
-public class PasswordOverviewActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Cursor> {
+public class PasswordOverviewActivity extends AppCompatActivity implements LoaderManager.LoaderCallbacks<Password> {
 
-    private ListViewCompat listView;
+    static final int PASSWORD_LOADER_ID = 0xCCCC;
+
+    private ListViewCompat passwordListViewCompat;
     private Toolbar toolbar;
-    private FloatingActionButton floatingActionButton;
+    private FloatingActionButton addPasswordFloatingActionButton;
+    private TextView noPasswordsTextView;
+
+    private PasswordOverviewAdapter passwordOverviewAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.password_list_layout);
 
-        listView = (ListViewCompat) findViewById(R.id.passwordlistlayout_listview_passwords);
-        toolbar = (Toolbar) findViewById(R.id.passworddetail_toolbar);
-        floatingActionButton = (FloatingActionButton) findViewById(R.id.passwordlistlayout_floatingactionbutton_add);
-        floatingActionButton.setOnClickListener(new AddPasswordCallback(this));
-    }
+        // get elements from XML-View
+        passwordListViewCompat = (ListViewCompat) findViewById(R.id.passwordlistlayout_listview_passwords);
+        toolbar = (Toolbar) findViewById(R.id.passwordlistlayout_toolbar);
+        addPasswordFloatingActionButton = (FloatingActionButton) findViewById(R.id.passwordlistlayout_floatingactionbutton_add);
+        noPasswordsTextView = (TextView) findViewById(R.id.passwordlistlayout_textview_nopasswords);
 
+        // set onClick-event to add new passwords
+        addPasswordFloatingActionButton.setOnClickListener(new AddPasswordCallback(this));
+
+        // ...
+        setSupportActionBar(toolbar);
+
+        // init and set adapter
+        passwordOverviewAdapter = new PasswordOverviewAdapter(this);
+        passwordListViewCompat.setAdapter(passwordOverviewAdapter);
+
+        // init background loader for passwords
+        getLoaderManager().initLoader(PASSWORD_LOADER_ID, null, this);
+    }
 
     @Override
-    public Loader<Cursor> onCreateLoader(int i, Bundle bundle) {
-        return null;
+    protected void onResume() {
+        super.onResume();
+        getLoaderManager().getLoader(PASSWORD_LOADER_ID).forceLoad();
     }
 
     @Override
-    public void onLoadFinished(Loader<Cursor> loader, Cursor cursor) {
-
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.password_list_menu, menu);
+        return true;
     }
 
     @Override
-    public void onLoaderReset(Loader<Cursor> loader) {
+    public boolean onOptionsItemSelected(MenuItem item) {
 
+        return true;
     }
+
+    @Override
+    public Loader<Password> onCreateLoader(int id, Bundle bundle) {
+        Loader<Password> loader = null;
+
+        switch (id) {
+            case PASSWORD_LOADER_ID:
+                String userId = Integer.toString(UserProvider.getInstance(this).getId());
+                loader = new AsyncPasswordLoader(this, DatabaseProvider.GET_ALL_PASSWORDS_BY_USER_ID, userId);
+                break;
+        }
+
+        return loader;
+    }
+
+    @Override
+    public void onLoadFinished(Loader<Password> loader, Password password) {
+        int id = loader.getId();
+        switch (id) {
+            case PASSWORD_LOADER_ID:
+
+                break;
+        }
+    }
+
+    @Override
+    public void onLoaderReset(Loader<Password> loader) {
+        int id = loader.getId();
+        switch (id) {
+            case PASSWORD_LOADER_ID:
+
+                break;
+        }
+    }
+
+
 }
--- app/src/main/java/core/Password.java
+++ app/src/main/java/core/Password.java
@@ -1,26 +1,33 @@
 package core;
 
+import android.text.style.TtsSpan;
+
 import java.text.DateFormat;
+import java.text.ParseException;
 import java.text.SimpleDateFormat;
 import java.util.ArrayList;
 import java.util.Date;
 import java.util.List;
+import java.util.Locale;
 
 public class Password {
     private int id;
+    private int position;
     private String username;
     private String program;
-    private List<String> passwordHistory;
+    private List<PasswordHistory> passwordHistory;
 
-    public Password(int id, String username, String program) {
+    public Password(int id, int position, String username, String program) {
         this.id = id;
+        this.position = position;
         this.username = username;
         this.program = program;
-        this.passwordHistory = new ArrayList<String>();
+        this.passwordHistory = new ArrayList<>();
     }
 
     public void logout() {
         id = Integer.MIN_VALUE;
+        position = Integer.MIN_VALUE;
         username = null;
         program = null;
         passwordHistory.clear();
@@ -28,15 +35,44 @@ public class Password {
     }
 
     public int addToHistory(String password) {
-        String date = getDate();
-
+        String date = Utils.getDate();
 
         return 0;
     }
 
-    private String getDate() {
-        DateFormat dateFormat = new SimpleDateFormat("yyyy-MM-dd");
-        Date date = new Date();
-        return dateFormat.format(date);
+    void addHistoryItem(int id, String value, String date) {
+        DateFormat dateFormat = new SimpleDateFormat("dd.MM.yyyy", Locale.getDefault());
+        Date convertedDate = new Date();
+        try {
+            convertedDate = dateFormat.parse(date);
+        } catch (ParseException e) {
+            e.printStackTrace();
+        }
+
+        PasswordHistory history = new PasswordHistory(id, value, convertedDate);
+        passwordHistory.add(history);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (!(o instanceof Password)) return false;
+
+        Password password = (Password) o;
+
+        if (id != password.id) return false;
+        if (position != password.position) return false;
+        if (!username.equals(password.username)) return false;
+        return program.equals(password.program);
+
+    }
+
+    @Override
+    public int hashCode() {
+        int result = id;
+        result = 31 * result + position;
+        result = 31 * result + username.hashCode();
+        result = 31 * result + program.hashCode();
+        return result;
     }
 }
