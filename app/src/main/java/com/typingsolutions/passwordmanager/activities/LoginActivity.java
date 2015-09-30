f498788f483132ba237c04356f685d661299069a
Clemens Gerstung <clemens8496@gmail.com>
2015-09-30 18:51:09 +0200

    ---

--- app/src/main/java/com/typingsolutions/passwordmanager/activities/LoginActivity.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/activities/LoginActivity.java
@@ -45,6 +45,7 @@ public class LoginActivity extends AppCompatActivity {
         add = (FloatingActionButton) findViewById(R.id.mainlayout_floatingactionbutton_add);
         add.setOnClickListener(new CreateUserCallback(this));
 
+        // TODO: yeah...
         getSupportFragmentManager()
                 .beginTransaction()
                 .replace(R.id.layout_to_replace, loginUsernameFragment)
