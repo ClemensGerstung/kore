f0f7e88bc0364daf6dcfe871320059a0d5349a98
Clemens Gerstung <clemens8496@gmail.com>
2015-10-05 19:03:59 +0200

    improved remember user

--- app/src/main/java/com/typingsolutions/passwordmanager/callbacks/ShowEnterPasswordCallback.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/callbacks/ShowEnterPasswordCallback.java
@@ -1,16 +1,19 @@
 package com.typingsolutions.passwordmanager.callbacks;
 
 import android.content.Context;
+import android.content.SharedPreferences;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.View;
 import com.typingsolutions.passwordmanager.R;
 import com.typingsolutions.passwordmanager.activities.LoginActivity;
 import com.typingsolutions.passwordmanager.fragments.LoginPasswordFragment;
+import com.typingsolutions.passwordmanager.fragments.LoginUsernameFragment;
 import core.UserProvider;
 
 public class ShowEnterPasswordCallback extends BaseCallback {
-
 
     private LoginActivity activity;
     private String username;
@@ -23,7 +26,7 @@ public class ShowEnterPasswordCallback extends BaseCallback {
     @Override
     public void onClick(View v) {
         FloatingActionButton floatingActionButton = (FloatingActionButton) v;
-        if(!UserProvider.getInstance(context).userExists(username)) {
+        if (!UserProvider.getInstance(context).userExists(username)) {
             Snackbar.make(v, "Sorry, your name doesn't exist", Snackbar.LENGTH_LONG)
                     .setAction("CREATE", new CreateUserCallback(context, username))
                     .show();
@@ -32,9 +35,17 @@ public class ShowEnterPasswordCallback extends BaseCallback {
 
         UserProvider.getInstance(context).setUsername(username);
 
-        activity.getSupportFragmentManager()
-                .beginTransaction()
-                .replace(R.id.layout_to_replace, new LoginPasswordFragment())
+        final SharedPreferences preferences = activity.getPreferences(Context.MODE_PRIVATE);
+        boolean rememberUser = preferences.getBoolean(LoginUsernameFragment.REMEMBER, false);
+
+        if (rememberUser) {
+            final SharedPreferences.Editor editor = preferences.edit();
+            editor.putString(LoginUsernameFragment.REMEMBERED_USERNAME, username);
+        }
+
+        FragmentManager fragmentManager = activity.getSupportFragmentManager();
+        FragmentTransaction transaction = fragmentManager.beginTransaction();
+        transaction.replace(R.id.layout_to_replace, new LoginPasswordFragment())
                 .commit();
 
         floatingActionButton.setImageResource(R.drawable.add);
@@ -43,7 +54,7 @@ public class ShowEnterPasswordCallback extends BaseCallback {
 
     @Override
     public void setValues(Object... values) {
-        if(values[0] instanceof String) {
+        if (values[0] instanceof String) {
             username = (String) values[0];
         }
     }
