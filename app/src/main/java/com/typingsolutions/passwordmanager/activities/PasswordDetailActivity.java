7d2333f482ac6a39552f197328285907dc73b8ac
Clemens Gerstung <clemens8496@gmail.com>
2015-10-16 22:05:11 +0200

    working on issue #25 - view is now completely done

--- app/src/main/java/com/typingsolutions/passwordmanager/activities/PasswordDetailActivity.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/activities/PasswordDetailActivity.java
@@ -1,19 +1,20 @@
 package com.typingsolutions.passwordmanager.activities;
 
+import android.app.ActionBar;
+import android.graphics.Rect;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.CardView;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
-import android.util.DisplayMetrics;
-import android.util.Log;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.ViewGroup;
-import android.view.ViewTreeObserver;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.*;
 import android.widget.EditText;
 import com.typingsolutions.passwordmanager.R;
+import com.typingsolutions.passwordmanager.callbacks.textwatcher.AddPasswordTextWatcher;
 import core.Password;
 import core.PasswordProvider;
 import core.adapter.PasswordHistoryAdapter;
@@ -27,12 +28,58 @@ public class PasswordDetailActivity extends AppCompatActivity {
     private EditText username;
     private EditText password;
     private CardView delete;
+    private CardView passwordHistoryCard;
+    private CardView passwordCard;
     private RecyclerView passwordHistory;
+
     private RecyclerView.LayoutManager layoutManager;
     private PasswordHistoryAdapter passwordHistoryAdapter;
 
+    private AddPasswordTextWatcher usernameTextWatcher;
+    private AddPasswordTextWatcher programTextWatcher;
+    private AddPasswordTextWatcher passwordTextWatcher;
+
     private int passwordId;
     private Password currentPassword;
+
+    private boolean first = true;
+    private View.OnLayoutChangeListener deleteLayoutChanged = new View.OnLayoutChangeListener() {
+        @Override
+        public void onLayoutChange(View v, int left, int top, int right, int bottom, int oldLeft, int oldTop, int oldRight, int oldBottom) {
+            int windowHeight = PasswordDetailActivity.this.getWindow().getDecorView().getHeight();
+
+            if (first) {
+                first = false;
+                if(bottom > windowHeight) return;
+
+                Rect passwordHistory = new Rect();
+                Rect password = new Rect();
+
+                passwordHistoryCard.getGlobalVisibleRect(passwordHistory);
+                passwordCard.getGlobalVisibleRect(password);
+
+                ViewGroup.MarginLayoutParams margin = (ViewGroup.MarginLayoutParams) passwordHistoryCard.getLayoutParams();
+
+                int toolbarHeight = Build.VERSION.SDK_INT >= 21 ? toolbar.getMeasuredHeight() : 0;
+                int additionalMargin = Build.VERSION.SDK_INT >= 21 ? (margin.topMargin * 2 - margin.bottomMargin) : 0;
+
+                int newDeletePos = windowHeight - delete.getMeasuredHeight();
+                int height = newDeletePos - additionalMargin - password.bottom - toolbarHeight;
+                ViewGroup.LayoutParams params = passwordHistoryCard.getLayoutParams();
+                params.height = height;
+                passwordHistoryCard.setLayoutParams(params);
+            }
+        }
+    };
+
+    private PasswordHistoryAdapter.OnItemAddedCallback onItemAddedCallback = new PasswordHistoryAdapter.OnItemAddedCallback() {
+        @Override
+        public void onItemAdded(PasswordHistoryAdapter.ViewHolder viewHolder, int position) {
+            ViewGroup.LayoutParams params = passwordHistory.getLayoutParams();
+            params.height = params.height + 1;
+            passwordHistory.setLayoutParams(params);
+        }
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -45,7 +92,15 @@ public class PasswordDetailActivity extends AppCompatActivity {
         password = (EditText) findViewById(R.id.passworddetaillayout_edittext_password);
         delete = (CardView) findViewById(R.id.passworddetaillayout_cardview_delete);
         passwordHistory = (RecyclerView) findViewById(R.id.passworddetaillayout_recyclerview_passwordhistory);
+        passwordHistoryCard = (CardView) findViewById(R.id.passworddetaillayout_cardview_passwordhistory);
+        passwordCard = (CardView) findViewById(R.id.passworddetaillayout_cardview_password);
 
+        toolbar.setNavigationOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                onBackPressed();
+            }
+        });
         setSupportActionBar(toolbar);
 
         passwordId = getIntent().getIntExtra(START_DETAIL_INDEX, -1);
@@ -56,25 +111,41 @@ public class PasswordDetailActivity extends AppCompatActivity {
         passwordHistoryAdapter = new PasswordHistoryAdapter(this, passwordId);
         passwordHistory.setLayoutManager(layoutManager);
         passwordHistory.setAdapter(passwordHistoryAdapter);
+        passwordHistoryAdapter.setOnItemAddedCallback(onItemAddedCallback);
 
-        program.setText(currentPassword.getProgram());
-        username.setText(currentPassword.getUsername());
-        password.setText(currentPassword.getFirstItem().getValue());
+        String programString = currentPassword.getProgram();
+        programTextWatcher = new AddPasswordTextWatcher(this, programString);
+        program.setText(programString);
+        program.addTextChangedListener(programTextWatcher);
 
+        String usernameString = currentPassword.getUsername();
+        usernameTextWatcher = new AddPasswordTextWatcher(this, usernameString);
+        username.setText(usernameString);
+        username.addTextChangedListener(usernameTextWatcher);
 
-    }
+        String passwordString = currentPassword.getFirstItem().getValue();
+        passwordTextWatcher = new AddPasswordTextWatcher(this, passwordString);
+        password.setText(passwordString);
+        password.addTextChangedListener(passwordTextWatcher);
 
-    @Override
-    protected void onResume() {
-        super.onResume();
-        
+        delete.addOnLayoutChangeListener(deleteLayoutChanged);
     }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         MenuInflater inflater = getMenuInflater();
         inflater.inflate(R.menu.create_user_menu, menu);
-
+        switchMenuState(false);
         return super.onCreateOptionsMenu(menu);
+    }
+
+    public void switchMenuState(boolean state) {
+        MenuItem item = toolbar.getMenu().getItem(0);
+        item.setEnabled(state);
+        if (state) {
+            item.getIcon().setAlpha(255);
+        } else {
+            item.getIcon().setAlpha(64);
+        }
     }
 }
