aed27107516936ce3d89d77378945f07ae204477
Clemens Gerstung <clemens8496@gmail.com>
2015-11-24 18:16:48 +0100

    "runnable state" --> won't work anyway but will compile

--- app/src/main/java/com/typingsolutions/passwordmanager/services/LoginService.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/services/LoginService.java
@@ -18,139 +18,115 @@ import java.security.NoSuchAlgorithmException;
 
 public class LoginService extends Service {
 
-    public static final String INTENT_ACTION = "com.typingsolutions.passwordmanager.service.LoginService.UPDATE_BLOCKING";
-    public static final String INTENT_BLOCK = "com.typingsolutions.passwordmanager.service.LoginService.BLOCK";
+  public static final String INTENT_ACTION = "com.typingsolutions.passwordmanager.service.LoginService.UPDATE_BLOCKING";
+  public static final String INTENT_BLOCK = "com.typingsolutions.passwordmanager.service.LoginService.BLOCK";
 
-    public static final int SLEEP_TIME = 1000;
+  public static final int SLEEP_TIME = 1000;
 
-    // tries until block
-    public static int TRIES_FOR_SMALL_BLOCK = 3;
-    public static int TRIES_FOR_MEDIUM_BLOCK = 6;
-    public static int TRIES_FOR_LARGE_BLOCK = 9;
-    public static int TRIES_FOR_FINAL_BLOCK = 12;
+  // tries until block
+  public static int TRIES_FOR_SMALL_BLOCK = 3;
+  public static int TRIES_FOR_MEDIUM_BLOCK = 6;
+  public static int TRIES_FOR_LARGE_BLOCK = 9;
+  public static int TRIES_FOR_FINAL_BLOCK = 12;
 
-    // block times in ms
-    public static int SMALL_BLOCK_TIME = 30000;    // 0.5 minutes
-    public static int MEDIUM_BLOCK_TIME = 60000;   // 1 minute
-    public static int LARGE_BLOCK_TIME = 150000;   // 2.5 minutes
-    public static int FINAL_BLOCK_TIME = 300000;   // 5 minutes
+  // block times in ms
+  public static int SMALL_BLOCK_TIME = 30000;    // 0.5 minutes
+  public static int MEDIUM_BLOCK_TIME = 60000;   // 1 minute
+  public static int LARGE_BLOCK_TIME = 150000;   // 2.5 minutes
+  public static int FINAL_BLOCK_TIME = 300000;   // 5 minutes
 
-    private final RemoteCallbackList<IServiceCallback> callbacks = new RemoteCallbackList<>();
-    private final BlockedUserList blockedUserList = new BlockedUserList(this);
+  private final RemoteCallbackList<IServiceCallback> callbacks = new RemoteCallbackList<>();
+  private final BlockedUserList blockedUserList = new BlockedUserList(this);
 
-    private final ILoginServiceRemote.Stub binder = new ILoginServiceRemote.Stub() {
+  private final ILoginServiceRemote.Stub binder = new ILoginServiceRemote.Stub() {
 
-        @Override
-        public boolean login(int id, String passwordHash, String dbPasswordHash) throws RemoteException {
-
-            if (!passwordHash.equals(dbPasswordHash)) {
-                blockedUserList.add(id);
-                return false;
-            }
-            blockedUserList.remove(id);
-
-            return true;
-        }
-
-        @Override
-        public void getBlockedTimeAsync(int id) throws RemoteException {
-            final int size = callbacks.beginBroadcast();
-
-            BlockedUser user = blockedUserList.getUserById(id);
-            if (user == null){
-                callbacks.finishBroadcast();
-                return;
-            }
-
-//            Log.d(getClass().getSimpleName(), String.format("Get block time async for %s callbacks", size));
-            for (int i = 0; i < size; i++) {
-                callbacks.getBroadcastItem(i).getLockTime(user.getTimeRemaining(), user.getCompleteTime());
-            }
-
-            callbacks.finishBroadcast();
-        }
-
-        @Override
-        @Deprecated
-        public boolean isUserBlocked(int id) throws RemoteException {
-            BlockedUser user = blockedUserList.getUserById(id);
-            return user != null && user.isBlocked();
-        }
-
-        @Override
-        @Deprecated
-        public int getMaxBlockTime(int id) throws RemoteException {
-            BlockedUser user = blockedUserList.getUserById(id);
-            if (user == null) return -1;
-            return user.getCompleteTime();
-        }
-
-        @Override
-        public void registerCallback(IServiceCallback callback) throws RemoteException {
-            if (callback != null) {
-                callbacks.register(callback);
-            }
-        }
-
-        @Override
-        public void unregisterCallback(IServiceCallback callback) throws RemoteException {
-            if (callback != null) {
-                callbacks.unregister(callback);
-            }
-        }
-    };
 
     @Override
-    public IBinder onBind(Intent intent) {
-        readSerializedData();
-        return binder;
+    public void increaseTries() throws RemoteException {
+
     }
 
     @Override
-    public void onRebind(Intent intent) {
-        readSerializedData();
+    public void getBlockedTimeAsync() throws RemoteException {
+
     }
 
     @Override
-    public boolean onUnbind(Intent intent) {
-        SharedPreferences preferences = getSharedPreferences(getClass().getSimpleName(), MODE_PRIVATE);
-        SharedPreferences.Editor editor = preferences.edit();
+    public boolean isUserBlocked() throws RemoteException {
+      return false;
+    }
 
-        try {
-            String json = blockedUserList.toJson();
-            String hash = Utils.getHashedString(json);
+    @Override
+    public int getMaxBlockTime() throws RemoteException {
+      return 0;
+    }
+
+    @Override
+    public void registerCallback(IServiceCallback callback) throws RemoteException {
+      if (callback != null) {
+        callbacks.register(callback);
+      }
+    }
+
+    @Override
+    public void unregisterCallback(IServiceCallback callback) throws RemoteException {
+      if (callback != null) {
+        callbacks.unregister(callback);
+      }
+    }
+  };
+
+  @Override
+  public IBinder onBind(Intent intent) {
+    readSerializedData();
+    return binder;
+  }
+
+  @Override
+  public void onRebind(Intent intent) {
+    readSerializedData();
+  }
+
+  @Override
+  public boolean onUnbind(Intent intent) {
+    SharedPreferences preferences = getSharedPreferences(getClass().getSimpleName(), MODE_PRIVATE);
+    SharedPreferences.Editor editor = preferences.edit();
+
+    try {
+      String json = blockedUserList.toJson();
+      String hash = Utils.getHashedString(json);
 
 //            Log.d(getClass().getSimpleName(), json);
 
-            editor.putString("json", json);
-            editor.putString("hash", hash);
+      editor.putString("json", json);
+      editor.putString("hash", hash);
 
-            editor.apply();
-        } catch (IOException | NoSuchAlgorithmException e) {
-            Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
-        }
-
-        return true;
+      editor.apply();
+    } catch (IOException | NoSuchAlgorithmException e) {
+      Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
     }
 
-    @Override
-    public int onStartCommand(Intent intent, int flags, int startId) {
-        readSerializedData();
-        return START_STICKY;
-    }
+    return true;
+  }
 
-    private void readSerializedData() {
-        SharedPreferences preferences = getSharedPreferences(getClass().getSimpleName(), MODE_PRIVATE);
-        String json = preferences.getString("json", "");
-        String hash = preferences.getString("hash", "");
+  @Override
+  public int onStartCommand(Intent intent, int flags, int startId) {
+    readSerializedData();
+    return START_STICKY;
+  }
 
-        try {
-            String computedHash = Utils.getHashedString(json);
-            blockedUserList.fromJson(json, hash.equals(computedHash));
-        } catch (NoSuchAlgorithmException | IOException e) {
-            Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
-        } finally {
-            preferences.edit().clear().apply();
-        }
+  private void readSerializedData() {
+    SharedPreferences preferences = getSharedPreferences(getClass().getSimpleName(), MODE_PRIVATE);
+    String json = preferences.getString("json", "");
+    String hash = preferences.getString("hash", "");
+
+    try {
+      String computedHash = Utils.getHashedString(json);
+      blockedUserList.fromJson(json, hash.equals(computedHash));
+    } catch (NoSuchAlgorithmException | IOException e) {
+      Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
+    } finally {
+      preferences.edit().clear().apply();
     }
+  }
 }
