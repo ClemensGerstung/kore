a17740dae9205d041a7d1d71088b2b162132c3c7
Clemens Gerstung <clemens8496@gmail.com>
2015-11-26 19:32:13 +0100

    login and setup improvements

--- app/src/main/java/core/async/AsyncPasswordLoader.java
+++ app/src/main/java/core/async/AsyncPasswordLoader.java
@@ -5,21 +5,21 @@ import android.os.AsyncTask;
 import android.util.Log;
 
 public class AsyncPasswordLoader extends AsyncTask<String, Void, Void> {
-    private Context context;
+  private Context context;
 
-    public AsyncPasswordLoader(Context context) {
-        super();
-        this.context = context;
+  public AsyncPasswordLoader(Context context) {
+    super();
+    this.context = context;
+  }
+
+  @Override
+  protected Void doInBackground(String... params) {
+    try {
+
+    } catch (Exception e) {
+      Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
     }
 
-    @Override
-    protected Void doInBackground(String... params) {
-        try {
-
-        } catch (Exception e) {
-            Log.e(getClass().getSimpleName(), String.format("%s: %s", e.getClass().getSimpleName(), e.getMessage()));
-        }
-
-        return null;
-    }
+    return null;
+  }
 }
