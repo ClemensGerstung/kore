b9813f28bbe4ca6cb7c338965211b8bc50db9243
Clemens Gerstung <clemens8496@gmail.com>
2015-12-10 23:03:29 +0100

    license dialog

--- app/src/main/java/core/adapter/AboutViewPagerAdapter.java
+++ app/src/main/java/core/adapter/AboutViewPagerAdapter.java
@@ -7,6 +7,7 @@ import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentPagerAdapter;
 import android.support.v4.app.ListFragment;
 import android.support.v4.content.ContextCompat;
+import android.text.SpannableString;
 import android.text.SpannableStringBuilder;
 import android.text.Spanned;
 import android.text.style.DynamicDrawableSpan;
@@ -19,31 +20,26 @@ public class AboutViewPagerAdapter extends FragmentPagerAdapter {
 
   private Context context;
   private Fragment[] contents = new Fragment[2];
+  private Drawable icon;
 
   public AboutViewPagerAdapter(Context context, FragmentManager fragmentManager) {
     super(fragmentManager);
     this.context = context;
-    contents[0] = new LicenseFragment();
-    contents[1] = new AboutFragment();
+    contents[1] = new LicenseFragment();
+    contents[0] = new AboutFragment();
   }
 
   @Override
   public Fragment getItem(int position) {
-    return contents[0];
+    return contents[position];
   }
 
   @Override
   public CharSequence getPageTitle(int position) {
-    SpannableStringBuilder spannableStringBuilder = new SpannableStringBuilder();
-    Drawable icon = position == 0
-        ? ContextCompat.getDrawable(context, R.mipmap.info)
-        : ContextCompat.getDrawable(context, R.mipmap.copyright);
-    icon.setBounds(5, 5, icon.getIntrinsicWidth(), icon.getIntrinsicHeight());
-    ImageSpan span = new ImageSpan(icon, DynamicDrawableSpan.ALIGN_BASELINE);
-
-    spannableStringBuilder.setSpan(span, 0, 1, Spanned.SPAN_EXCLUSIVE_EXCLUSIVE);
-    return super.getPageTitle(position);
+    return "";
   }
+
+
 
   @Override
   public int getCount() {
