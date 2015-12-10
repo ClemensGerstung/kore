9027702aa099fd1204a850ac7b42a319d7d9605d
Clemens Gerstung <clemens8496@gmail.com>
2015-12-10 22:05:03 +0100

    about dialog tab layout and fragments

--- app/src/main/java/com/typingsolutions/passwordmanager/activities/AboutActivity.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/activities/AboutActivity.java
@@ -2,31 +2,31 @@ package com.typingsolutions.passwordmanager.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.design.widget.TabLayout;
+import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
-import android.webkit.WebView;
+import android.support.v7.widget.Toolbar;
 import com.typingsolutions.passwordmanager.R;
-
-import java.io.InputStream;
+import core.adapter.AboutViewPagerAdapter;
 
 
 public class AboutActivity extends AppCompatActivity {
+  private Toolbar toolbar_actionbar;
+  private TabLayout tabLayout_navigation;
+  private ViewPager viewpager_content;
+
   @Override
   protected void onCreate(@Nullable Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
-    setContentView(R.layout.about_layout);
-    WebView webView = (WebView) findViewById(R.id.aboutlayout_webview_about);
-    InputStream rawStream = getResources().openRawResource(R.raw.about);
+    setContentView(R.layout.about_license_layout);
+    toolbar_actionbar = (Toolbar) findViewById(R.id.aboutlayout_toolbar_actionbar);
+    tabLayout_navigation = (TabLayout) findViewById(R.id.aboutlayout_tablayout_navigation);
+    viewpager_content = (ViewPager) findViewById(R.id.aboutlayout_viewpager_content);
 
-    try {
-      if(rawStream == null || rawStream.available() == 0)
-        throw new IllegalStateException("Couldn't read source");
+    setSupportActionBar(toolbar_actionbar);
+    AboutViewPagerAdapter adapter = new AboutViewPagerAdapter(this, getSupportFragmentManager());
 
-      final byte[] data = new byte[rawStream.available()];
-      rawStream.read(data);
-      String html = new String(data);
-      webView.loadData(html, "text/html", "UTF-8");
-    } catch (Exception e) {
-
-    }
+    viewpager_content.setAdapter(adapter);
+    tabLayout_navigation.setupWithViewPager(viewpager_content);
   }
 }
