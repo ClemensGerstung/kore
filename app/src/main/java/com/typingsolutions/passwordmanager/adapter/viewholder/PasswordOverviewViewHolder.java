c3f13246f8c6e7753180cc827e870a60288f5ff1
Clemens Gerstung <clemens8496@gmail.com>
2016-01-03 18:57:35 +0100

    --- refactoring PasswordOverviewStuff

--- app/src/main/java/com/typingsolutions/passwordmanager/adapter/viewholder/PasswordOverviewViewHolder.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/adapter/viewholder/PasswordOverviewViewHolder.java
@@ -1,11 +1,8 @@
 package com.typingsolutions.passwordmanager.adapter.viewholder;
 
-import android.content.Context;
 import android.content.DialogInterface;
 import android.content.Intent;
 import android.support.v7.app.AlertDialog;
-import android.support.v7.widget.CardView;
-import android.support.v7.widget.EditTextImpl;
 import android.support.v7.widget.RecyclerView;
 import android.view.View;
 import android.view.ViewManager;
@@ -13,16 +10,15 @@ import android.widget.EditText;
 import android.widget.TextView;
 import com.typingsolutions.passwordmanager.R;
 import com.typingsolutions.passwordmanager.activities.PasswordDetailActivity;
+import com.typingsolutions.passwordmanager.activities.PasswordOverviewActivity;
 import core.DatabaseProvider;
-import com.typingsolutions.passwordmanager.adapter.PasswordOverviewAdapter;
 
 
 public class PasswordOverviewViewHolder extends RecyclerView.ViewHolder
     implements View.OnClickListener, DialogInterface.OnClickListener,
-    IItemTouchHelperViewHolder {
+    IItemTouchHelperViewHolder, DatabaseProvider.OnOpenListener {
 
-  private Context context;
-  private PasswordOverviewAdapter passwordOverviewAdapter;
+  private PasswordOverviewActivity activity;
 
   public final TextView program;
   public final TextView username;
@@ -31,11 +27,10 @@ public class PasswordOverviewViewHolder extends RecyclerView.ViewHolder
   public int id;
   private boolean safe = false;
 
-  public PasswordOverviewViewHolder(final Context context, PasswordOverviewAdapter passwordOverviewAdapter, final View itemView) {
+  public PasswordOverviewViewHolder(final PasswordOverviewActivity activity, final View itemView) {
     super(itemView);
 
-    this.context = context;
-    this.passwordOverviewAdapter = passwordOverviewAdapter;
+    this.activity = activity;
 
     program = (TextView) itemView.findViewById(R.id.passwordlistitemlayout_textview_program);
     username = (TextView) itemView.findViewById(R.id.passwordlistitemlayout_textview_username);
@@ -44,7 +39,7 @@ public class PasswordOverviewViewHolder extends RecyclerView.ViewHolder
     icon.setOnClickListener(new View.OnClickListener() {
       @Override
       public void onClick(View v) {
-        AlertDialog dialog = new AlertDialog.Builder(context)
+        AlertDialog dialog = new AlertDialog.Builder(activity)
             .setTitle("asdf")
             .create();
         dialog.show();
@@ -65,17 +60,14 @@ public class PasswordOverviewViewHolder extends RecyclerView.ViewHolder
   @Override
   public void onClick(View v) {
     if (safe) {
-      AlertDialog dialog = new AlertDialog.Builder(context)
+      AlertDialog dialog = new AlertDialog.Builder(activity)
           .setTitle("Reenter your password")
           .setView(R.layout.reenter_password_layout)
           .setPositiveButton("OK", this)
           .create();
       dialog.show();
     } else {
-      Intent intent = new Intent(context, PasswordDetailActivity.class);
-      intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
-      intent.putExtra(PasswordDetailActivity.START_DETAIL_INDEX, id);
-      context.startActivity(intent);
+      startDetailActivity();
     }
   }
 
@@ -85,25 +77,43 @@ public class PasswordOverviewViewHolder extends RecyclerView.ViewHolder
     EditText editText = (EditText) alert.findViewById(R.id.reenterpasswordlayout_edittext_password);
     String password = editText.getText().toString();
 
-    passwordOverviewAdapter.setRefreshing(true);
-    DatabaseProvider.getConnection(context)
-        .tryOpen(password, passwordOverviewAdapter.getOnOpenListener());
-    passwordOverviewAdapter.setCurrentId(id);
+    activity.setRefreshing(true);
+    DatabaseProvider.getConnection(activity).tryOpen(password, null);
 
     alert.dismiss();
   }
 
   @Override
   public void onItemSelected() {
-    CardView cardView = (CardView) itemView;
-    cardView.setCardElevation(12.0f);
-    cardView.setCardBackgroundColor(0x99FAFAFA);
+    itemView.setBackgroundColor(0x99FAFAFA);
   }
 
   @Override
   public void onItemClear() {
-    CardView cardView = (CardView) itemView;
-    cardView.setCardElevation(0.0f);
-    cardView.setCardBackgroundColor(0x00FAFAFA);
+    itemView.setBackgroundColor(0x00FAFAFA);
+  }
+
+  @Override
+  public void open() {
+    activity.setRefreshing(false);
+    startDetailActivity();
+  }
+
+
+  /**
+   * @hide
+   */
+  private void startDetailActivity() {
+    Intent intent = new Intent(activity, PasswordDetailActivity.class);
+    intent.setFlags(Intent.FLAG_ACTIVITY_NO_HISTORY);
+    intent.putExtra(PasswordDetailActivity.START_DETAIL_INDEX, id);
+    activity.doNotLogout();
+    activity.startActivity(intent);
+  }
+
+  @Override
+  public void refused() {
+    activity.setRefreshing(false);
+    activity.makeSnackBar("Your passwords do not match");
   }
 }
