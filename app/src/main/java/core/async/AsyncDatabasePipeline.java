36057041129edb02130cd3345c5b80c116fb3780
Clemens Gerstung <clemens8496@gmail.com>
2016-01-26 17:57:46 +0100

    async db query

--- app/src/main/java/core/async/AsyncDatabasePipeline.java
+++ app/src/main/java/core/async/AsyncDatabasePipeline.java
@@ -2,39 +2,80 @@ package core.async;
 
 import android.content.Context;
 import android.util.Log;
+import android.util.Pair;
+import core.DatabaseProvider;
 import core.Dictionary;
+import net.sqlcipher.Cursor;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
+import java.util.Scanner;
 
 public class AsyncDatabasePipeline {
   private Context context;
-  private Dictionary<String, Object[]> queries;
+  private Dictionary<Pair<String, Object[]>, AsyncQueryListener> queries;
   private boolean working;
+  private boolean exit;
   private final Thread looper;
 
   private final Runnable looping = new Runnable() {
     @Override
     public void run() {
-      working = true;
+      if (exit)
+        return;
+      Dictionary.Element<Pair<String, Object[]>, AsyncQueryListener> element = null;
+      DatabaseProvider provider = DatabaseProvider.getConnection(context);
       synchronized (AsyncDatabasePipeline.this.looper) {
-        Dictionary.Element element = queries.getFirstIterator();
+        element = queries.getFirstIterator();
+        working = queries.hasNext();
+      }
 
-        while (queries.hasNext()) {
-          // TODO: Run DB-Query
-          Log.d(AsyncDatabasePipeline.class.getSimpleName(), String.format("%s => %s", element.getKey(), Arrays.toString((Object[]) element.getValue())));
+      while (working) {
 
-          queries.removeByKey((String) element.getKey());
-          element = queries.next();
-        }
         try {
-          looper.wait();
-        } catch (InterruptedException ignored) {
+          String query = element.getKey().first.toUpperCase();
+          if (query.startsWith("INSERT")) {
+            long result = provider.insert(element.getKey().first, element.getKey().second);
+            raiseActionListener(element.getValue(), result >= 0, "INSERT result is -1!", result);
+          } else if (query.startsWith("UPDATE") || query.startsWith("REMOVE")) {
+            long result = provider.update(element.getKey().first, element.getKey().second);
+            raiseActionListener(element.getValue(), result >= 0, "UPDATE/REMOVE result is -1!", result);
+          } else if (query.startsWith("SELECT")) {
+            Cursor cursor = provider.query(element.getKey().first, (String[]) element.getKey().second);
+            raiseActionListener(element.getValue(), cursor != null, "Couldn't query", cursor);
+          } else {
+            throw new IllegalArgumentException("No known SQLiteQuery");
+          }
+        } catch (Exception e) {
+          raiseActionListener(element.getValue(), false, e.getMessage());
+        }
+
+
+        synchronized (AsyncDatabasePipeline.this.looper) {
+          element = queries.removeFirst();
+          working = queries.hasElements();
         }
       }
-      working = false;
+
+      try {
+        looper.wait();
+        looper.run();
+      } catch (InterruptedException ignored) {
+      }
     }
   };
 
+  private void raiseActionListener(AsyncQueryListener listener, boolean additionalCondition, String failMessage, Object... results) {
+    if (listener == null)
+      return;
+
+    if (additionalCondition) {
+      listener.executed(results);
+    } else {
+      listener.failed(failMessage);
+    }
+  }
 
   private static AsyncDatabasePipeline PIPELINE;
 
@@ -46,7 +87,7 @@ public class AsyncDatabasePipeline {
   }
 
   public void addQuery(String query, Object... params) {
-    queries.addLast(query, params);
+    //queries.addLast(query, params);
     if (!working) {
       if (!looper.isAlive()) {
         looper.start();
@@ -76,5 +117,9 @@ public class AsyncDatabasePipeline {
     this.looper = new Thread(looping);
   }
 
+  public interface AsyncQueryListener {
+    void executed(Object... results);
 
+    void failed(String message);
+  }
 }
