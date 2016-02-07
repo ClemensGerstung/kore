7ccd38fd1154deb9622ed891a770f733869fa9ca
Clemens Gerstung <clemens8496@gmail.com>
2016-02-07 01:09:35 +0100

    ---

--- app/src/main/java/com/typingsolutions/passwordmanager/activities/AboutActivity.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/activities/AboutActivity.java
@@ -2,14 +2,9 @@ package com.typingsolutions.passwordmanager.activities;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
-import android.support.design.widget.TabLayout;
-import android.support.v4.view.ViewPager;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.view.View;
 import com.typingsolutions.passwordmanager.R;
-import com.typingsolutions.passwordmanager.adapter.AboutViewPagerAdapter;
-import com.typingsolutions.passwordmanager.callbacks.click.ToolbarNavigationCallback;
 
 
 public class AboutActivity extends AppCompatActivity {
