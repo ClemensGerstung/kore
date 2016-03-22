4e28c41f736f0f21b8c765de9e93b266ad773888
Clemens Gerstung <clemens8496@gmail.com>
2016-03-22 22:19:11 +0100

    ---

--- app/src/main/java/com/typingsolutions/passwordmanager/fragments/SetupPasswordFragment.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/fragments/SetupPasswordFragment.java
@@ -9,18 +9,33 @@ import android.widget.Button;
 import com.typingsolutions.passwordmanager.BaseFragment;
 import com.typingsolutions.passwordmanager.R;
 import com.typingsolutions.passwordmanager.activities.SetupActivity;
+import com.typingsolutions.passwordmanager.callbacks.SetupPasswordCallback;
 import com.typingsolutions.passwordmanager.callbacks.SwitchFragmentCallback;
 
 public class SetupPasswordFragment extends BaseFragment<SetupActivity> {
+  private static final SetupWelcomeFragment SETUP_WELCOME_FRAGMENT = new SetupWelcomeFragment();
+
+  private Button mButtonAsGoBack;
+  private Button mButtonAsSetupPassword;
+
+  private SwitchFragmentCallback mCallbackAsGoBack;
+  private SetupPasswordCallback mCallbackAsSetupPassword;
+
+
   @Nullable
   @Override
   public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
     View view = inflater.inflate(R.layout.setup_2_layout, container, false);
-    Button b = (Button) view.findViewById(R.id.setuplayout_button_prev);
 
-    SwitchFragmentCallback callback = new SwitchFragmentCallback(getSupportActivity(), R.id.setuplayout_fragment_wrapper,
-        new SetupWelcomeFragment(), R.anim.slide_in_left, R.anim.slide_out_right);
-    b.setOnClickListener(callback);
+    mCallbackAsGoBack = new SwitchFragmentCallback(getSupportActivity(), R.id.setuplayout_fragment_wrapper, SETUP_WELCOME_FRAGMENT, R.anim.slide_in_left, R.anim.slide_out_right);
+    mCallbackAsSetupPassword = new SetupPasswordCallback(getSupportActivity(), R.id.setuplayout_fragment_wrapper, null, R.anim.slide_in_right, R.anim.slide_out_left);
+
+    mButtonAsGoBack = (Button) view.findViewById(R.id.setuplayout_button_prev);
+    mButtonAsSetupPassword = (Button) view.findViewById(R.id.setuplayout_button_setuppassword);
+
+    mButtonAsGoBack.setOnClickListener(mCallbackAsGoBack);
+    mButtonAsSetupPassword.setOnClickListener(mCallbackAsSetupPassword);
+
     return view;
   }
 
