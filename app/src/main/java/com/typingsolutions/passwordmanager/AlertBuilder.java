47ca998422fdb8c0b83a3ebb45ae4d2cb1036ebb
Clemens Gerstung <clemens8496@gmail.com>
2016-07-11 11:22:38 +0200

    AlertBuilder memoryleak fixes -> activity reusing -> TODO: cleanup in method onStop()

---
+++ app/src/main/java/com/typingsolutions/passwordmanager/AlertBuilder.java
@@ -0,0 +1,106 @@
+package com.typingsolutions.passwordmanager;
+
+import android.app.AlertDialog;
+import android.content.Context;
+import android.content.DialogInterface;
+import android.support.annotation.LayoutRes;
+import android.support.annotation.NonNull;
+import android.support.annotation.StyleRes;
+import android.view.View;
+import android.view.WindowManager;
+import android.widget.Button;
+
+public class AlertBuilder {
+  private static AlertBuilder builder = new AlertBuilder();
+
+  private AlertDialog mAlertDialog;
+  private CharSequence mPositiveText = null;
+  private CharSequence mNegativeText = null;
+  private CharSequence mNeutralText = null;
+
+
+  public static AlertBuilder create(Context context) {
+    return create(context, 0);
+  }
+
+  public static AlertBuilder create(Context context, @StyleRes int theme) {
+    builder.mAlertDialog = new AlertDialog.Builder(context, theme).create();
+    return builder;
+  }
+
+  public static AlertDialog getLastCreated() {
+    return builder.mAlertDialog;
+  }
+
+  private AlertBuilder setButton(int which, CharSequence text) {
+    mAlertDialog.setButton(which, text, (DialogInterface.OnClickListener) null);
+    return this;
+  }
+
+  public AlertBuilder setPositiveButton(CharSequence text) {
+    mPositiveText = text;
+    return this;
+  }
+
+  public AlertBuilder setNegativeButton(CharSequence text) {
+    mNegativeText = text;
+    return this;
+  }
+
+  public AlertBuilder setNeutralButton(CharSequence text) {
+    mNeutralText = text;
+    return this;
+  }
+
+  /**
+   * Need to be called after {@see setPositiveButton}, {@see setNegativeButton} or {@see setNeutralButton}
+   *
+   * @param callback BaseDialogCallback
+   * @return AlertBuilder
+   */
+  public AlertBuilder setCallback(@NonNull BaseDialogCallback<? extends BaseActivity> callback) {
+    mAlertDialog.setOnShowListener(callback);
+
+    if (mPositiveText != null) {
+      mAlertDialog.setButton(AlertDialog.BUTTON_POSITIVE, mPositiveText, callback);
+    }
+
+    if (mNegativeText != null) {
+      mAlertDialog.setButton(AlertDialog.BUTTON_NEGATIVE, mNegativeText, callback);
+    }
+
+    if (mNeutralText != null) {
+      mAlertDialog.setButton(AlertDialog.BUTTON_NEUTRAL, mNeutralText, callback);
+    }
+
+    return this;
+  }
+
+  public AlertBuilder setView(@LayoutRes int layout) {
+    return setView(View.inflate(mAlertDialog.getContext(), layout, null));
+  }
+
+  public AlertBuilder setView(View view) {
+    mAlertDialog.setView(view);
+    return this;
+  }
+
+  public AlertBuilder setSecurityFlags() {
+    mAlertDialog.getWindow().setFlags(WindowManager.LayoutParams.FLAG_SECURE, WindowManager.LayoutParams.FLAG_SECURE);
+    return this;
+  }
+
+  public AlertBuilder setTitle(CharSequence title) {
+    mAlertDialog.setTitle(title);
+    return this;
+  }
+
+  public AlertBuilder setMessage(CharSequence message) {
+    mAlertDialog.setMessage(message);
+    return this;
+  }
+
+  public void show() {
+    mAlertDialog.show();
+  }
+}
--- app/src/main/java/com/typingsolutions/passwordmanager/callbacks/LoginDialogCallback.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/callbacks/LoginDialogCallback.java
@@ -1,7 +1,7 @@
 package com.typingsolutions.passwordmanager.callbacks;
 
 import android.content.DialogInterface;
-import android.support.v7.app.AlertDialog;
+import android.app.AlertDialog;
 import android.widget.EditText;
 import com.typingsolutions.passwordmanager.BaseDialogCallback;
 import com.typingsolutions.passwordmanager.R;
