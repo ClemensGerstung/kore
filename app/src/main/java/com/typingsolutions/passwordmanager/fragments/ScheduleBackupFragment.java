4b7b077921f4f656d83a65433fe80b789e3a611a
Clemens Gerstung <clemens8496@gmail.com>
2016-08-20 14:25:48 +0200

    backup file padding

--- app/src/main/java/com/typingsolutions/passwordmanager/fragments/ScheduleBackupFragment.java
+++ app/src/main/java/com/typingsolutions/passwordmanager/fragments/ScheduleBackupFragment.java
@@ -8,30 +8,47 @@ import android.content.Intent;
 import android.content.SharedPreferences;
 import android.net.ConnectivityManager;
 import android.net.NetworkInfo;
+import android.net.wifi.WifiManager;
+import android.os.AsyncTask;
 import android.os.Bundle;
 import android.support.annotation.NonNull;
 import android.support.annotation.Nullable;
+import android.text.TextUtils;
 import android.util.Log;
-import android.widget.GridLayout;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import com.google.android.gms.common.AccountPicker;
+import android.widget.GridLayout;
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GoogleApiAvailability;
+import com.google.api.client.extensions.android.http.AndroidHttp;
 import com.google.api.client.googleapis.extensions.android.gms.auth.GoogleAccountCredential;
+import com.google.api.client.googleapis.extensions.android.gms.auth.GooglePlayServicesAvailabilityIOException;
+import com.google.api.client.googleapis.extensions.android.gms.auth.UserRecoverableAuthIOException;
+import com.google.api.client.http.FileContent;
+import com.google.api.client.http.HttpTransport;
+import com.google.api.client.json.JsonFactory;
+import com.google.api.client.json.jackson2.JacksonFactory;
 import com.google.api.client.util.ExponentialBackOff;
+import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
+import com.google.api.services.drive.model.File;
+import com.google.api.services.drive.model.FileList;
 import com.typingsolutions.passwordmanager.BaseFragment;
 import com.typingsolutions.passwordmanager.R;
 import com.typingsolutions.passwordmanager.activities.BackupActivity;
+import com.typingsolutions.passwordmanager.database.DatabaseConnection;
 import pub.devrel.easypermissions.AfterPermissionGranted;
 import pub.devrel.easypermissions.EasyPermissions;
 
+import java.io.IOException;
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
 import java.util.List;
 
 import static android.app.Activity.RESULT_OK;
+import static android.content.Context.WIFI_SERVICE;
 
 
 public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
@@ -41,7 +58,7 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
   static final int REQUEST_GOOGLE_PLAY_SERVICES = 1002;
   static final int REQUEST_PERMISSION_GET_ACCOUNTS = 1003;
   private static final String PREF_ACCOUNT_NAME = "accountName";
-  private static final String[] SCOPES = { DriveScopes.DRIVE_METADATA_READONLY };
+  private static final String[] SCOPES = {DriveScopes.DRIVE_FILE};
 
   GoogleAccountCredential mCredential;
 
@@ -53,8 +70,7 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
     GridLayout l = (GridLayout) root.findViewById(R.id.backuplayout_layout_chooseaccount);
     l.setOnClickListener(this::onChooseAccountClick);
 
-    mCredential = GoogleAccountCredential.usingOAuth2(
-        getActivity().getApplicationContext(), Arrays.asList(SCOPES))
+    mCredential = GoogleAccountCredential.usingOAuth2(getActivity().getApplicationContext(), Arrays.asList(SCOPES))
         .setBackOff(new ExponentialBackOff());
 
     return root;
@@ -81,6 +97,7 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
       } else {
         Intent intent = mCredential.newChooseAccountIntent();
         intent.putExtra("overrideTheme", 1);
+        intent.putExtra("customTheme", 0);
         startActivityForResult(intent, REQUEST_ACCOUNT_PICKER);
       }
     } else {
@@ -96,13 +113,14 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
     } else if (!isDeviceOnline()) {
       getSupportActivity().makeSnackbar("No network connection available.");
     } else {
-//      new MakeRequestTask(mCredential).execute();
+      new MakeRequestTask(mCredential).execute();
     }
   }
 
   private boolean isDeviceOnline() {
     ConnectivityManager connMgr = (ConnectivityManager) getContext().getSystemService(Context.CONNECTIVITY_SERVICE);
     NetworkInfo networkInfo = connMgr.getActiveNetworkInfo();
+    // TODO: check WIFI
     return (networkInfo != null && networkInfo.isConnected());
   }
 
@@ -129,7 +147,7 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
   @Override
   public void onActivityResult(int requestCode, int resultCode, Intent data) {
     super.onActivityResult(requestCode, resultCode, data);
-    switch(requestCode) {
+    switch (requestCode) {
       case REQUEST_GOOGLE_PLAY_SERVICES:
         if (resultCode != RESULT_OK) {
           Log.d(getClass().getSimpleName(), "This app requires Google Play Services. Please install Google Play Services on your device and relaunch this app.");
@@ -166,5 +184,92 @@ public class ScheduleBackupFragment extends BaseFragment<BackupActivity>
   @Override
   public void onPermissionsDenied(int requestCode, List<String> perms) {
 
+  }
+
+  private class MakeRequestTask extends AsyncTask<Void, Void, List<String>> {
+    private Drive mService = null;
+    private Exception mLastError = null;
+
+    public MakeRequestTask(GoogleAccountCredential credential) {
+      HttpTransport transport = AndroidHttp.newCompatibleTransport();
+      JsonFactory jsonFactory = JacksonFactory.getDefaultInstance();
+      mService = new Drive.Builder(transport, jsonFactory, credential)
+          .setApplicationName("kore")
+          .build();
+    }
+
+    @Override
+    protected List<String> doInBackground(Void... params) {
+      try {
+        return getDataFromApi();
+      } catch (Exception e) {
+        mLastError = e;
+        cancel(true);
+        return null;
+      }
+    }
+
+    private List<String> getDataFromApi() throws IOException {
+      List<String> fileInfo = new ArrayList<>();
+
+      File folderMetadata = new File();
+      folderMetadata.setName("kore");
+      folderMetadata.setMimeType("application/vnd.google-apps.folder");
+
+      List<File> results = mService.files().list().setQ("name='kore' and trashed=false").execute().getFiles();
+      File folder = null;
+
+      if (results.isEmpty()) {
+        folder = mService.files()
+            .create(folderMetadata)
+            .setFields("id, name")
+            .execute();
+
+        fileInfo.add(String.format("%s (%s)", folder.getName(), folder.getId()));
+      } else {
+        folder = results.get(0);
+      }
+
+      java.io.File local = getContext().getDatabasePath(DatabaseConnection.DATABASE_NAME);
+      FileContent localContent = new FileContent("application/octet-stream", local);
+      File remote = new File();
+      remote.setName("backup-123");
+      remote.setDescription("This is backup file created by the awesome password manager kore.");
+      remote.setParents(Collections.singletonList(folder.getId()));
+
+      File file = mService.files()
+          .create(remote, localContent)
+          .setFields("id, parents")
+          .execute();
+
+      Log.d(getClass().getSimpleName(), "Uploaded " + file.getId());
+
+      return fileInfo;
+    }
+
+    @Override
+    protected void onPostExecute(List<String> output) {
+      if (output == null || output.size() == 0) {
+        Log.d(getClass().getSimpleName(), "No results returned.");
+      } else {
+        output.add(0, "Data retrieved using the Drive API:");
+        Log.d(getClass().getSimpleName(), TextUtils.join("\n", output));
+      }
+    }
+
+    @Override
+    protected void onCancelled() {
+      if (mLastError == null) {
+        Log.d(getClass().getSimpleName(), "Request cancelled.");
+      } else {
+        if (mLastError instanceof GooglePlayServicesAvailabilityIOException) {
+          showGooglePlayServicesAvailabilityErrorDialog(((GooglePlayServicesAvailabilityIOException) mLastError).getConnectionStatusCode());
+        } else if (mLastError instanceof UserRecoverableAuthIOException) {
+          startActivityForResult(((UserRecoverableAuthIOException) mLastError).getIntent(), REQUEST_AUTHORIZATION);
+        } else {
+          Log.e(getClass().getSimpleName(), "The following error occurred:" + mLastError.getMessage());
+        }
+      }
+    }
   }
 }
